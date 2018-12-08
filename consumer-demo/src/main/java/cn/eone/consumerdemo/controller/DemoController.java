8362ef5da0f9aeab19051ae47c831b9127a1cc2f
彭兆峰 <absorprofess@outlook.com>
2018-12-08 10:48:44 +0800

    commit

--- consumer-demo/src/main/java/cn/eone/consumerdemo/controller/DemoController.java
+++ consumer-demo/src/main/java/cn/eone/consumerdemo/controller/DemoController.java
@@ -1,7 +1,5 @@
 package cn.eone.consumerdemo.controller;
 
-
-
 import cn.eone.service.DemoService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
