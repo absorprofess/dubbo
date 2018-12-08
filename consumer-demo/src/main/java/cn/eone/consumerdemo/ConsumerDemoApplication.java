9c55a8996b5dff13a24f9f02f221d2c32a30344a
彭兆峰 <absorprofess@outlook.com>
2018-12-08 14:24:44 +0800

    commit

--- consumer-demo/src/main/java/cn/eone/consumerdemo/ConsumerDemoApplication.java
+++ consumer-demo/src/main/java/cn/eone/consumerdemo/ConsumerDemoApplication.java
@@ -4,9 +4,8 @@ import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication
 @EnableDubboConfiguration
+@SpringBootApplication
 public class ConsumerDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerDemoApplication.class, args);
